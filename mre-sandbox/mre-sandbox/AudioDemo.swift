5d6cb893ca43b599b26645ab77495edfc828fd3c
Mike Eberhardt <mike.r.eberhardt@gmail.com>
2026-02-10 15:13:49 -0700

    feat: add an audio demo with play/pause and volume control, integrating it into the product view.

---
+++ mre-sandbox/mre-sandbox/AudioDemo.swift
@@ -0,0 +1,18 @@
+//
+//  AudioDemo.swift
+//  mre-sandbox
+//
+//  Created by Eberhardt Macbook Air on 8/15/24.
+//
+
+import SwiftUI
+
+struct AudioDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AudioDemo()
+}
